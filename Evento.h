dad61482445f82080307e5f7a2c46db6a6aee2b7
FerG17 <fguerec17@gmail.com>
2025-06-12 00:57:33 -0500

    Evento.h

---
+++ Evento.h
@@ -0,0 +1,51 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include "SeccionLugar.h"
+using namespace std;
+
+class Evento {
+protected:
+    static int nextId;
+    int id;
+    string nombre;
+    string fecha;
+    string lugar;
+    double precio;
+    SeccionLugar seccionPrincipal;
+    SeccionLugar seccionGeneral;
+    SeccionLugar seccionVIP;
+public:
+    Evento()
+        : id(nextId++), nombre(""), fecha(""), lugar(""), precio(0.0),
+        seccionPrincipal("Principal", 10),
+        seccionGeneral("General", 10),
+        seccionVIP("VIP Gold", 5) {
+    }
+
+    Evento(const string& nombre, const string& fecha, const string& lugar, double precio,
+        int asientosPrincipal = 10, int asientosGeneral = 10, int asientosVIP = 5)
+        : id(nextId++), nombre(nombre), fecha(fecha), lugar(lugar), precio(precio),
+        seccionPrincipal("Principal", asientosPrincipal),
+        seccionGeneral("General", asientosGeneral),
+        seccionVIP("VIP Gold", asientosVIP) {
+    }
+
+    virtual ~Evento() {}
+
+    int getId() const { return id; }
+    string getNombre() const { return nombre; }
+    string getFecha() const { return fecha; }
+    string getLugar() const { return lugar; }
+    double getPrecio() const { return precio; }
+    SeccionLugar* getSeccionPrincipal() { return &seccionPrincipal; }
+    SeccionLugar* getSeccionGeneral() { return &seccionGeneral; }
+    SeccionLugar* getSeccionVIP() { return &seccionVIP; }
+    virtual void mostrar(int x = 45, int y = 18) const {
+        gotoxy(x, y++); cout << "ID: " << id;
+        gotoxy(x, y++); cout << "Nombre: " << nombre;
+        gotoxy(x, y++); cout << "Fecha:  " << fecha;
+        gotoxy(x, y++); cout << "Lugar:  " << lugar;
+        gotoxy(x, y++); cout << "Precio base: S/ " << precio;
+    }
+};
