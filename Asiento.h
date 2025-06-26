7de19a032196ed6c145a7bc43555ada80b54f9b8
FerG17 <fguerec17@gmail.com>
2025-06-26 11:08:11 -0500

    Asiento.h

--- Asiento.h
+++ Asiento.h
@@ -1,7 +1,6 @@
 #pragma once
 #include <iostream>
-#include "conio.h"
-using namespace std;
+#include <fstream>
 
 class Asiento {
 private:
@@ -9,10 +8,26 @@ private:
     bool disponible;
 public:
     Asiento() : numero(0), disponible(true) {}
+
     Asiento(int numero) : numero(numero), disponible(true) {}
 
     int getNumero() const { return numero; }
+
     bool estaDisponible() const { return disponible; }
+
     void reservar() { disponible = false; }
+
     void liberar() { disponible = true; }
+
+    // Serialización para guardar en archivo
+    void guardar(std::ofstream& out) const {
+        out << numero << " " << disponible << "\n";
+    }
+
+    // Deserialización para cargar desde archivo
+    void cargar(std::ifstream& in) {
+        int disp;
+        in >> numero >> disp;
+        disponible = (disp != 0);
+    }
 };
