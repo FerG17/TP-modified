2b22e68900b7c6900f180b84adbe9efa0e8491cd
FerG17 <fguerec17@gmail.com>
2025-06-28 00:05:33 -0500

    Lugar.h

--- Lugar.h
+++ Lugar.h
@@ -1,13 +1,23 @@
 #pragma once
+#include <string>
 #include "SeccionLugar.h"
 
+using namespace std;
+
 class Lugar {
+private:
     string nombre;
-    SeccionLugar* seccion;
+    SeccionLugar principal;
+    SeccionLugar general;
+    SeccionLugar vip;
 public:
-    Lugar(string nom) : nombre(nom), seccion(new SeccionLugar("General")) {}
-    void mostrarLugar() {
-        cout << "Lugar: " << nombre << endl;
-        seccion->mostrarAsientos();
+    Lugar(const string& nombre, int asientosPrincipal, int asientosGeneral, int asientosVIP)
+        : nombre(nombre), principal("Principal", asientosPrincipal),
+        general("General", asientosGeneral), vip("VIP Gold", asientosVIP) {
     }
+
+    // Getters para secciones
+    SeccionLugar& getPrincipal() { return principal; }
+    SeccionLugar& getGeneral() { return general; }
+    SeccionLugar& getVIP() { return vip; }
 };
