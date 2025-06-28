dad388ab62e385a10aca67c74681bdcdb0cbe04d
FerG17 <fguerec17@gmail.com>
2025-06-28 00:06:59 -0500

    ObraTeatral.h

--- ObraTeatral.h
+++ ObraTeatral.h
@@ -1,26 +1,20 @@
 #pragma once
 #include "Evento.h"
+#include <string>
+using namespace std;
 
 class ObraTeatral : public Evento {
 private:
     string director;
-    int duracionMin;
-
 public:
-    // Constructor compatible con el de Evento
     ObraTeatral(const string& nombre, const string& fecha, const string& lugar, double precio,
-        const string& director, int duracion,
-        int asientosPrincipal = 10, int asientosGeneral = 10, int asientosVIP = 5)
+        const string& director, int asientosPrincipal = 10, int asientosGeneral = 10, int asientosVIP = 5)
         : Evento(nombre, fecha, lugar, precio, asientosPrincipal, asientosGeneral, asientosVIP),
-        director(director), duracionMin(duracion) {
+        director(director) {
     }
 
-    void mostrar(int x=45, int y = 18) const override {
-        cout << "\n   |   Obra Teatral: " << nombre << endl;
-        cout << "   |   Director: " << director << endl;
-        cout << "   |   Duracion: " << duracionMin << " min" << endl;
-        cout << "   |   Fecha: " << fecha << endl;
-        cout << "   |   Lugar: " << lugar << endl;
-        cout << "   |   Precio: S/." << precio << "\n" << endl;
+    void mostrar(int x = 45, int y = 18) const override {
+        Evento::mostrar(x, y);
+        cout << "Director: " << director << endl;
     }
 };
